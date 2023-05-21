560f46883edb184f1fd465b1aa0c32e7dfd22930
kohei0517 <losthound.kei127@gmail.com>
2023-05-21 13:51:33 +0900

    ユーザー管理機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,19 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   validates :nickname,presence: true
-  validates :name,presence: true
-  validates :f_name,presence: true
-  validates :name_kana,presence: true
-  validates :f_name_kana,presence: true
   validates :birthday,presence: true
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'に全角文字を使用してください' } do
+    validates :name
+    validates :f_name
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ン]+\z/, message: 'は全角カタカナを使用してください' } do
+    validates :name_kana
+    validates :f_name_kana
+  end
+
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'は半角英数を両方含む必要があります'}
 
 end
