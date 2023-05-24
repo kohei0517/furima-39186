6aa2e605c2e24cc1cafeb297f6d572f1d6b07694
kohei0517 <losthound.kei127@gmail.com>
2023-05-24 21:59:48 +0900

    出品登録の確認

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,4 +3,23 @@ class ItemsController < ApplicationController
     
   end
 
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  
+  def item_params
+    params.require(:item).permit(:title, :description, :category_id, :state_id, :payed_id, :area_id, :progress_id, :price, :image).merge(user_id: current_user.id)
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -9,9 +9,16 @@ class Item < ApplicationRecord
 
 
   validates :title, :description, :price, presence: true
+#  validates :image, attached_file_presence: true
   validates :category_id, numericality: { other_than: 1 ,message: "can't be blank"}
   validates :state_id, numericality: { other_than: 1 ,message: "can't be blank"}
   validates :payed_id, numericality: { other_than: 1 ,message: "can't be blank"}
   validates :area_id, numericality: { other_than: 1 ,message: "can't be blank"}
   validates :progress_id, numericality: { other_than: 1 ,message: "can't be blank"}
+
+  has_one_attached :image
+
+  def was_attached?
+    self.image.attached?
+  end
 end
