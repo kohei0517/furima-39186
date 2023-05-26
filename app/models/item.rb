3840404a839b5ec3562082dba37cc00c438370b7
kohei0517 <losthound.kei127@gmail.com>
2023-05-26 20:22:31 +0900

    モデル単体テスト

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,7 +8,8 @@ class Item < ApplicationRecord
   belongs_to :state
 
 
-  validates :title, :description, :price, presence: true
+  validates :title,  length: { maximum: 40 }, presence: true
+  validates :description,  length: { maximum: 1000 }, presence: true
   validates :price, presence: true, format: { with: /\A[0-9]+\z/ }, numericality: true, inclusion: { in: 300..9_999_999 }
   validates :image, presence: true
   validates :category_id, numericality: { other_than: 1 ,message: "can't be blank"}
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,94 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品登録' do
+    context '商品登録出来る時' do
+      it '全ての情報が揃えば登録出来る' do
+        expect(@item).to be_valid
+      end
+    end
+
+    context '商品登録出来ない時' do
+      it '商品画像が必須であること' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+
+      end
+      it '商品名が必須であること' do
+        @item.title = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Title can't be blank")
+      end
+      it '商品名が40文字以内であること' do
+        @item.title = 10**41
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Title is too long (maximum is 40 characters)")
+      end
+      it '商品の説明が必須であること' do
+        @item.description = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Description can't be blank")
+      end
+      it '商品の説明が1000文字以下であること' do
+        @item.description = 10**1001
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Description is too long (maximum is 1000 characters)")
+      end
+      it 'カテゴリーが必須であること' do
+        @item.category_id = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+      it '商品の状態が必須であること' do
+        @item.state_id = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("State can't be blank")
+      end
+      it '配送料の負担が必須であること' do        
+        @item.payed_id = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Payed can't be blank")
+      end
+      it '発送元の地域が必須であること' do
+        @item.area_id = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Area can't be blank")
+      end
+      it "発送までの日数が必須であること" do
+        @item.progress_id = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Progress can't be blank")
+      end
+      it "価格が必須であること" do
+        @item.price = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+      it "価格が299以下では不可能である事" do
+        @item.price = 299
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not included in the list",)
+      end
+      it "価格が10000000以上では不可能であること" do
+        @item.price = 10000000
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not included in the list",)
+      end
+      it "価格が半角数字である事" do
+        @item.price = "あいうえお"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+      it "ユーザーが必須であること" do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("User must exist")
+      end
+    end
+  end
 end
