bcddbf92f213d721a4a484c24ff44515bf9dbb04
kohei0517 <129520325+kohei0517@users.noreply.github.com>
2023-05-28 20:36:29 +0900

    Revert "編集機能の実装"

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,5 @@
 class ItemsController < ApplicationController
   before_action :go_signin ,only: :new
-  before_action :call_item, only: [ :show, :edit, :update]
-  before_action :equal_user, only: :edit
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -12,6 +10,7 @@ class ItemsController < ApplicationController
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def create
@@ -23,22 +22,7 @@ class ItemsController < ApplicationController
     end
   end
 
-  def edit
-  end
-
-  def update
-    if @item.update(item_params)
-      redirect_to item_path(params[:id])
-    else
-      render :edit
-    end
-  end
-
   private
-
-  def call_item
-    @item = Item.find(params[:id])
-  end
   
   def item_params
     params.require(:item).permit(:title, :description, :category_id, :state_id, :payed_id, :area_id, :progress_id, :price, :image).merge(user_id: current_user.id)
@@ -47,12 +31,6 @@ class ItemsController < ApplicationController
   def go_signin
     unless user_signed_in?
       redirect_to new_user_session_path
-    end
-  end
-
-  def equal_user
-    unless current_user.id == @item.user.id
-      redirect_to root_path
     end
   end
 end
