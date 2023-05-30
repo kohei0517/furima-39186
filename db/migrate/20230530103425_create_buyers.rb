af91c7bb01a31701a09e2220c286f26d80e36d07
kohei0517 <losthound.kei127@gmail.com>
2023-05-30 19:40:52 +0900

    マイグレートファイルの作成

--- db/migrate/20230530103425_create_buyers.rb
+++ db/migrate/20230530103425_create_buyers.rb
@@ -1,7 +1,13 @@
 class CreateBuyers < ActiveRecord::Migration[6.0]
   def change
     create_table :buyers do |t|
-
+      t.string     :zip_code, null: false
+      t.integer    :area_id,  null: false
+      t.string     :city,     null: false
+      t.string     :address,  null: false
+      t.string     :building
+      t.string     :phone,    null: false
+      t.references :buy,      null: false, foreign_key: true
       t.timestamps
     end
   end
